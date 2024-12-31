40aa01bf2eea99ac2103c6419f16a6cfdd213811
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-31 21:22:14 +0200

    feat: add averaging color in a window

--- app/src/main/java/com/cynex/colorhunt/core/ColorAnalyzer.kt
+++ app/src/main/java/com/cynex/colorhunt/core/ColorAnalyzer.kt
@@ -1,11 +1,12 @@
 package com.cynex.colorhunt.core
 
-import android.graphics.ImageFormat
 import android.graphics.PixelFormat
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import java.nio.ByteBuffer
+import kotlin.math.max
+import kotlin.math.min
 
 class ColorAnalyzer(private val listener: ColorChangeListener, private val averagingZone: Float): ImageAnalysis.Analyzer {
     override fun analyze(image: ImageProxy) {
@@ -18,14 +19,36 @@ class ColorAnalyzer(private val listener: ColorChangeListener, private val avera
         val plane = image.planes[0]
         val pixelStride = plane.pixelStride
         val rowStride = plane.rowStride
-        val centerPixel = pixelStride * (image.width / 2) + rowStride * (image.height / 2)
         val buffer: ByteBuffer = plane.buffer
 
-        val r = buffer.get(centerPixel).toInt() and 0xFF
-        val g = buffer.get(centerPixel + 1).toInt() and 0xFF
-        val b = buffer.get(centerPixel + 2).toInt() and 0xFF
+        val centerX = image.width / 2
+        val centerY = image.height / 2
 
-        val color = String.format("#%02x%02x%02x", r, g, b)
+        val windowSize = (minOf(image.width, image.height) * averagingZone).toInt()
+        val halfWindow = windowSize / 2
+        val startX = max(0, centerX - halfWindow)
+        val endX = min(image.width, centerX + halfWindow)
+        val startY = max(0, centerY - halfWindow)
+        val endY = min(image.height, centerY + halfWindow)
+
+        var rTotal = 0;
+        var gTotal = 0;
+        var bTotal = 0;
+        var pixelCount = 0;
+
+        for (y in startY until endY) {
+            for (x in startX until endX) {
+                val pixelIndex = pixelStride * x + rowStride * y
+
+                rTotal += buffer.get(pixelIndex).toInt() and 0xFF
+                gTotal += buffer.get(pixelIndex + 1).toInt() and 0xFF
+                bTotal += buffer.get(pixelIndex + 2).toInt() and 0xFF
+                pixelCount++;
+            }
+        }
+
+
+        val color = String.format("#%02x%02x%02x", rTotal/pixelCount, gTotal/pixelCount, bTotal/pixelCount)
         listener.onColorChanged(color)
         image.close()
     }
