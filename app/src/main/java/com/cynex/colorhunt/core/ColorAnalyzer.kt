16e36320e5412822c471da67fd45eb5044a5f122
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-31 21:59:46 +0200

    feat: add color change debouncing

--- app/src/main/java/com/cynex/colorhunt/core/ColorAnalyzer.kt
+++ app/src/main/java/com/cynex/colorhunt/core/ColorAnalyzer.kt
@@ -4,11 +4,16 @@ import android.graphics.PixelFormat
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.ColorUtils
 import java.nio.ByteBuffer
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.sqrt
 
 class ColorAnalyzer(private val listener: ColorChangeListener, private val averagingZone: Float): ImageAnalysis.Analyzer {
+    var prevColor: Triple<Int,Int,Int>? = null
+
     override fun analyze(image: ImageProxy) {
         if (image.format != PixelFormat.RGBA_8888) {
             Log.e("ColorAnalyzer", "Unsupported image format: ${image.format}")
@@ -47,11 +52,30 @@ class ColorAnalyzer(private val listener: ColorChangeListener, private val avera
             }
         }
 
+        val r = rTotal / pixelCount
+        val g = gTotal / pixelCount
+        val b = bTotal / pixelCount
+        val delta = prevColor?.let { prev -> calculateColorDelta(Triple(r, g, b), prev) }
 
-        val color = String.format("#%02x%02x%02x", rTotal/pixelCount, gTotal/pixelCount, bTotal/pixelCount)
-        listener.onColorChanged(color)
+        if (delta != null && delta >= 1.0) {
+            val color = String.format("#%02x%02x%02x", r, g, b)
+            listener.onColorChanged(color)
+        }
+        prevColor = Triple(r, g, b)
         image.close()
     }
+}
+
+private fun calculateColorDelta(a: Triple<Int, Int, Int>, b: Triple<Int, Int, Int>): Double {
+    val color1 = DoubleArray(3)
+    ColorUtils.RGBToLAB(a.first, a.second, a.third, color1)
+    val color2 = DoubleArray(3)
+    ColorUtils.RGBToLAB(b.first, b.second, b.third, color2)
+
+    val deltaL = color1[0] - color2[0]
+    val deltaA = color1[1] - color2[1]
+    val deltaB = color1[2] - color2[2]
+    return sqrt(deltaL * deltaL + deltaA * deltaA + deltaB * deltaB)
 }
 
 interface ColorChangeListener {
