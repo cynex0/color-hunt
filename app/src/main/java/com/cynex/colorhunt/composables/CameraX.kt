d9e3804bca199eef4098bfa92ab163f7d7e4c592
Maksims Orlovs <m.orlov10@gmail.com>
2024-12-31 18:06:47 +0200

    fix: center color value in box

--- app/src/main/java/com/cynex/colorhunt/composables/CameraX.kt
+++ app/src/main/java/com/cynex/colorhunt/composables/CameraX.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
-import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
@@ -13,19 +12,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -56,7 +60,7 @@ fun CameraPreviewScreen() {
             )
         }
 
-    Scaffold () { paddingValues: PaddingValues ->
+    Scaffold { paddingValues: PaddingValues ->
         Column (
             modifier = Modifier.fillMaxSize().padding(paddingValues),
             verticalArrangement = Arrangement.Center,
@@ -90,22 +94,31 @@ fun CameraPreviewScreen() {
                             }
                         }, ContextCompat.getMainExecutor(context))
                     }
-                }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .aspectRatio(4f / 3f)
             )
-        }
 
-        currentColor.value?.let {
-            Surface(
-                color = Color(android.graphics.Color.parseColor(it)),
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Text(
-                    text = it,
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
+            currentColor.value?.let {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .width(180.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color(android.graphics.Color.parseColor(it)))
+                        .wrapContentSize(Alignment.Center)
+                ) {
+                    Text(
+                        text = it,
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
             }
         }
     }
